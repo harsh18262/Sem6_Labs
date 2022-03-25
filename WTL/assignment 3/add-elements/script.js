0e6bcc01b1ebc7ed4e2e3e1638553b1aa87d59d8
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2022-03-25 11:14:50 +0530

    Update

---
+++ WTL/assignment 3/add-elements/script.js
@@ -0,0 +1,6 @@
+function addButton() {
+    var btn = document.createElement('p');
+    btn.className = "btn";
+    btn.innerHTML = "New Button <br> dscsd";
+    document.body.appendChild(btn);
+}
---
+++ WTL/assignment 3/color-picker-bootsrap/index.js
@@ -0,0 +1,9 @@
+let colorpicker = document.getElementById('colorpicker');
+
+
+
+  setInterval(()=>{
+      let color = colorpicker.value;
+      document.body.style.backgroundColor = color;
+  }
+  );
